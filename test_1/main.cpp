1c308a69d866c1e69d7fb6f78c57971b47aac365
zhou.tuan <zhou.tuan@gd-linux.com>
2020-09-22 19:53:18 +0800

    添加翻译功能

--- test_1/main.cpp
+++ test_1/main.cpp
@@ -1,16 +1,24 @@
 #include <QApplication>
 #include <QDialog>
 #include <QLabel>
+#include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString path = qApp->applicationDirPath();
+    qInfo() <<"path:"<<path;
+    QTranslator tsor;
+    tsor.load(QLocale(),"test", "_",path);
+    a.installTranslator(&tsor);
+
     QDialog w;
     QLabel label(&w);
     if(argc == 2)
         label.setText(argv[1]);
     else
-        label.setText("Helle World!");
+        label.setText(QObject::tr("Helle World!"));
     w.show();
     return a.exec();
 }
--- test_1/test_1.pro
+++ test_1/test_1.pro
@@ -1,3 +1,17 @@
 QT  += core gui widgets
+
 SOURCES += \
     main.cpp
+
+TRANSLATIONS = test_zh_CN.ts
+qtPrepareTool(LRELEASE, lrelease)
+for(tsfile, TRANSLATIONS) {
+    qmfile = $$shadowed($$tsfile)
+    qmfile ~= s,.ts$,.qm,
+    qmdir = $$dirname(qmfile)
+    !exists($$qmdir) {
+        mkpath($$qmdir)|error("Aborting.")
+    }
+    command = $$LRELEASE -removeidentical $$tsfile -qm $$qmfile
+    system($$command)|error("Failed to run: $$command")
+}
